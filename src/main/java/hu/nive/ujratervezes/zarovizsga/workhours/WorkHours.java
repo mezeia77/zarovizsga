9e9bcb029a0684cb4f03055ac624167be6fc410b
mezeia77 <mezeia77@gmail.com>
2021-03-16 18:34:06 +0100

    workhours

---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHours.java
@@ -0,0 +1,51 @@
+package hu.nive.ujratervezes.zarovizsga.workhours;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WorkHours {
+
+    public String minWork(String file) {
+        String result = "";
+        List<String> names = new ArrayList<>();
+        List<Integer> hours = new ArrayList<>();
+        List<String> dates = new ArrayList<>();
+
+        try (BufferedReader br = Files.newBufferedReader(Path.of(file))){
+            String line = "";
+            while ((line = br.readLine())!=null){
+                String [] parts = line.split(",");
+
+                String name = parts[0];
+                int hour = Integer.parseInt(parts[1]);
+                String date = parts[2];
+
+                names.add(name);
+                hours.add(hour);
+                dates.add(date);
+
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("File not found");
+        }
+
+        int minHours = Integer.MAX_VALUE;
+        String minName = "";
+        String minDate = "";
+
+        for (int i=0; i<hours.size(); i++){
+            if(hours.get(i)<minHours){
+                minHours = hours.get(i);
+                minName = names.get(i);
+                minDate = dates.get(i);
+            }
+        }
+
+        result = minName + ": " + minDate;
+        return result;
+    }
+}
--- src/test/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHoursTest.java
+++ src/test/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHoursTest.java
@@ -7,8 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class WorkHoursTest {
     @Test
     void minWork() {
-//        WorkHours workHours = new WorkHours();
-//        String result = workHours.minWork("src/test/resources/hu/nive/ujratervezes/zarovizsga/workhours/workhours.txt");
-//        assertEquals("John Doe: 2021-01-04", result);
+        WorkHours workHours = new WorkHours();
+        String result = workHours.minWork("src/test/resources/hu/nive/ujratervezes/zarovizsga/workhours/workhours.txt");
+        assertEquals("John Doe: 2021-01-04", result);
     }
 }
