ea62ff70e3cdb9c08de95ebba2a3f5412a99b26d
mezeia77 <mezeia77@gmail.com>
2021-03-16 19:19:45 +0100

    dogtypes

---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/dogtypes/DogTypes.java
@@ -0,0 +1,37 @@
+package hu.nive.ujratervezes.zarovizsga.dogtypes;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DogTypes {
+    DataSource dataSource;
+
+    public DogTypes(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public List<String> getDogsByCountry(String country) {
+        List<String> result = new ArrayList<>();
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement stmt =
+                     conn.prepareStatement("select name from dog_types where country = ? "))
+        {
+            stmt.setString(1, country);
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    String name = rs.getString("name");
+                    result.add(name.toLowerCase());
+                }
+            } catch (SQLException sqlException) {
+                throw new IllegalStateException("Cannot execute", sqlException);
+            }
+
+        } catch (SQLException sqlException) {
+            throw new IllegalStateException("No data found", sqlException);
+        }
+        return result;
+    }
+}
--- src/test/java/hu/nive/ujratervezes/zarovizsga/dogtypes/DogTypesTest.java
+++ src/test/java/hu/nive/ujratervezes/zarovizsga/dogtypes/DogTypesTest.java
@@ -14,28 +14,28 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class DogTypesTest {
     DogTypes dogTypes;
 
-//    @BeforeEach
-//    void init() throws SQLException {
-//        MariaDbDataSource dataSource;
-//        dataSource = new MariaDbDataSource();
-//        dataSource.setUrl("jdbc:mariadb://localhost:3306/employees?useUnicode=true");
-//        dataSource.setUser("employees");
-//        dataSource.setPassword("12345");
-//
-//        Flyway flyway = Flyway.configure().dataSource(dataSource).load();
-//
-//        flyway.clean();
-//        flyway.migrate();
-//
-//        dogTypes = new DogTypes(dataSource);
-//
-//    }
-//
-//    @Test
-//    void testGetDogsByCountry() {
-//        List<String> types = dogTypes.getDogsByCountry("Hungary");
-//        assertEquals(9, types.size());
-//        assertTrue(types.contains("komondor"));
-//        assertTrue(types.contains("kuvasz"));
-//    }
+    @BeforeEach
+    void init() throws SQLException {
+        MariaDbDataSource dataSource;
+        dataSource = new MariaDbDataSource();
+        dataSource.setUrl("jdbc:mariadb://localhost:3306/employees?useUnicode=true");
+        dataSource.setUser("root");
+        dataSource.setPassword("12345");
+
+        Flyway flyway = Flyway.configure().dataSource(dataSource).load();
+
+        flyway.clean();
+        flyway.migrate();
+
+        dogTypes = new DogTypes(dataSource);
+
+    }
+
+    @Test
+    void testGetDogsByCountry() {
+        List<String> types = dogTypes.getDogsByCountry("Hungary");
+        assertEquals(9, types.size());
+        assertTrue(types.contains("komondor"));
+        assertTrue(types.contains("kuvasz"));
+    }
 }
